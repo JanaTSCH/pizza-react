b0775753be4f2a8520c87b8e2d003876c242e06c
Jana-script <jana.jscript@gmail.com>
2025-03-07 19:04:50 +0100

    User state with redux toolkit

--- fast-react-pizza/src/features/cart/Cart.jsx
+++ fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
 
 const fakeCart = [
   {
@@ -27,6 +28,7 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector(state=>state.user.username);
   const cart = fakeCart;
   console.log(cart);
 
@@ -34,7 +36,7 @@ function Cart() {
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold capitalize">Your cart, {username}</h2>
 
       <ul className="divide-y divide-stone-300 border-b">
         {cart.map((item) => (
